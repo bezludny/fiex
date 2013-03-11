dc1b769c7b20ffa517fa58f279e248860a73e6f6
bezludny <bezludny@gmail.com>
2013-03-11 23:52:07 +0200

    Start commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiex::Application.config.secret_token = '41621dceb84d90a0056171687ad15a5ac86e32732adcfead178f43db9ae6bbda964978eb2a958e26d3006938d22057925002e44817fd789f2eb477ed89b4dc93'
